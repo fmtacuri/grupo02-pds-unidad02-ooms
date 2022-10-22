228a24bf2f93f689a3dab376ccf7082f252d5574
fmtacuri <56895072+fmtacuri@users.noreply.github.com>
2022-10-22 13:30:18 -0500

    Add files via upload

---
+++ src/main/java/com/grupo02/PdsUnidad02OomsApplication.java
@@ -0,0 +1,13 @@
+package com.grupo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsUnidad02OomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdsUnidad02OomsApplication.class, args);
+	}
+
+}
